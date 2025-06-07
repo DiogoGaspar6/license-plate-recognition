e72b3c3d5fb8bfecf8abf55d4c02e8eb25179fce
Diogo Gaspar <diogo.gaspar228@gmail.com>
2025-06-07 10:47:32 +0100

    stuff

--- models/portuguese license plates.v2i.yolov11/test-imagem.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO('runs/detect/matriculas-modelo3/weights/best.pt')
-
-
-results = model('imagem/imagem.jpg')
-
-# Mostra o resultado com bounding boxes
-results[0].show()
--- models/portuguese-license-plate.v1i.yolov11/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# My First Project > 2025-05-24 1:48pm
-https://universe.roboflow.com/licenseplateportugueserecognition/my-first-project-fgtm1
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
